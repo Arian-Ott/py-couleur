4eb24aebc6eb7004283b97d18ffbb0fbdd7e3bd2
Arian-Ott <88984301+Arian-Ott@users.noreply.github.com>
2025-01-26 12:39:31 +0100

    Add .gitignore and setup.py for py-couleur library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py-couleur',
+      version='0.1',
+      description='Python Colour library',
+      url='http://github.com/Arian-Ott/py-couleur',
+      author='Arian Ott',
+      author_email='',
+      license='MIT',
+      packages=['couleur'],
+      zip_safe=False)
